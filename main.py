1901a193d283b6a8b30d8c1f762e2f7cdef27a7f
David Castro <davidcastrosi@hotmail.com>
2022-06-15 15:12:02 -0600

    List comprehension for missing_states

--- main.py
+++ main.py
@@ -9,17 +9,13 @@ turtle.shape(image)
 data = pandas.read_csv("mexico-x-y.csv")
 state_names = data.estado.to_list()
 guessed_states = []
-missing_states = []
 
 while len(guessed_states) < 32:
     answer = screen.textinput(
         f"{len(guessed_states)}/32 Adivina el estado", "Cual es el nombre del estado?"
     ).title()
     if answer == "Exit":
-        for state in state_names:
-            if state not in guessed_states:
-                missing_states.append(state)
-
+        missing_states = [state for state in state_names if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("States to learn.csv")
         break
